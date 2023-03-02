daf4e8d11f07447008be15294c4cc521df294197
Aminat27 <adebayoaminat27@gmail.com>
2023-03-02 14:56:22 +0100

    added 6

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - display root, then left and finally right;
+ * @tree: pointer to the root node of the tree to transverse
+ * @func: pointer to function who takes as parameter the value in the node
+ *
+ * Return: void
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
